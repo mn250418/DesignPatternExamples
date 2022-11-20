cefaa5d70c1a65a57b0263e22ba1560775c02bf9
mn250418 <117892535+mn250418@users.noreply.github.com>
2022-11-20 15:58:54 +0530

    Add files via upload

---
+++ ObserverPattern/ObserverPattern/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class JobPost
+    {
+        public string Title { get; private set; }
+
+        public JobPost(string title)
+        {
+            Title = title;
+        }
+    }
+    class JobSeeker : IObserver<JobPost>
+    {
+        public string Name { get; private set; }
+
+        public JobSeeker(string name)
+        {
+            Name = name;
+        }
+
+        public void OnCompleted()
+        {
+            
+        }
+        public void OnError(Exception error)
+        {
+            
+        }
+
+        public void OnNext(JobPost value)
+        {
+            Console.WriteLine("Hi {0} ! New job posted: {1}", Name, value.Title);
+        }
+    }
+
+    class JobPostings : IObservable<JobPost>
+    {
+        private List<IObserver<JobPost>> mObservers;
+        private List<JobPost> mJobPostings;
+
+        public JobPostings()
+        {
+            mObservers = new List<IObserver<JobPost>>();
+            mJobPostings = new List<JobPost>();
+        }
+
+        public IDisposable Subscribe(IObserver<JobPost> observer)
+        {
+            // Check whether observer is already registered. If not, add it
+            if (!mObservers.Contains(observer))
+            {
+                mObservers.Add(observer);
+            }
+            return new Unsubscriber<JobPost>(mObservers, observer);
+        }
+
+        private void Notify(JobPost jobPost)
+        {
+            foreach (var observer in mObservers)
+            {
+                observer.OnNext(jobPost);
+            }
+        }
+
+        public void AddJob(JobPost jobPost)
+        {
+            mJobPostings.Add(jobPost);
+            Notify(jobPost);
+        }
+
+    }
+
+    internal class Unsubscriber<JobPost> : IDisposable
+    {
+        private List<IObserver<JobPost>> mObservers;
+        private IObserver<JobPost> mObserver;
+
+        internal Unsubscriber(List<IObserver<JobPost>> observers, IObserver<JobPost> observer)
+        {
+            this.mObservers = observers;
+            this.mObserver = observer;
+        }
+
+        public void Dispose()
+        {
+            if (mObservers.Contains(mObserver))
+                mObservers.Remove(mObserver);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Subscribers
+            var johnDoe = new JobSeeker("John");
+            var janeDoe = new JobSeeker("Rick");
+
+            //Create publisher and attch subscribers
+            var jobPostings = new JobPostings();
+            jobPostings.Subscribe(johnDoe);
+            jobPostings.Subscribe(janeDoe);
+
+            //Add a new job and see if subscribers get notified
+            jobPostings.AddJob(new JobPost("Software Engineer"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VisitorPattern/VisitorPattern/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    // Visitee
+    interface IAnimal
+    {
+        void Accept(IAnimalOperation operation);
+    }
+
+    // Visitor
+    interface IAnimalOperation
+    {
+        void VisitMonkey(Monkey monkey);
+        void VisitLion(Lion lion);
+        void VisitDolphin(Dolphin dolphin);
+    }
+
+    class Monkey : IAnimal
+    {
+        public void Shout()
+        {
+            Console.WriteLine("Oooh o aa aa!");
+        }
+
+        public void Accept(IAnimalOperation operation)
+        {
+            operation.VisitMonkey(this);
+        }
+    }
+
+    class Lion : IAnimal
+    {
+        public void Roar()
+        {
+            Console.WriteLine("Roaar!");
+        }
+
+        public void Accept(IAnimalOperation operation)
+        {
+            operation.VisitLion(this);
+        }
+    }
+
+    class Dolphin : IAnimal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Tuut tittu tuutt!");
+        }
+
+        public void Accept(IAnimalOperation operation)
+        {
+            operation.VisitDolphin(this);
+        }
+    }
+
+    class Speak : IAnimalOperation
+    {
+        public void VisitDolphin(Dolphin dolphin)
+        {
+            dolphin.Speak();
+        }
+
+        public void VisitLion(Lion lion)
+        {
+            lion.Roar();
+        }
+
+        public void VisitMonkey(Monkey monkey)
+        {
+            monkey.Shout();
+        }
+    }
+
+    class Jump : IAnimalOperation
+    {
+        public void VisitDolphin(Dolphin dolphin)
+        {
+            Console.WriteLine("Walked on water a little and disappeared!");
+        }
+
+        public void VisitLion(Lion lion)
+        {
+            Console.WriteLine("Jumped 7 feet! Back on the ground!");
+        }
+
+        public void VisitMonkey(Monkey monkey)
+        {
+            Console.WriteLine("Jumped 20 feet high! on to the tree!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var monkey = new Monkey();
+            var lion = new Lion();
+            var dolphin = new Dolphin();
+
+            var speak = new Speak();
+
+            monkey.Accept(speak);    
+            lion.Accept(speak);     
+            dolphin.Accept(speak);   
+
+            var jump = new Jump();
+
+            monkey.Accept(speak);  
+            monkey.Accept(jump);    
+
+            lion.Accept(speak);     
+            lion.Accept(jump);     
+
+            dolphin.Accept(speak);  
+            dolphin.Accept(jump);   
+
+            Console.ReadLine();
+        }
+    }
+}
